cdd4db533e38f2d1a7e219bcf50abf9f6a72a610
IbrahimEzzat3 <eng.ibrahim.ezzat.03@gmail.com>
2025-05-05 19:18:09 +0300

    deploy

--- api/application.py
+++ api/application.py
@@ -3,27 +3,20 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-'''
- secret_key using python3 secrets module
-'''
-app.secret_key = "9d367b3ba8e8654c6433379763e80c6e"
+# Secret key
+app.secret_key = os.getenv("SECRET_KEY", "9d367b3ba8e8654c6433379763e80c6e")
 
-'''
-Learn about virtualenv here:
-https://www.youtube.com/watch?v=N5vscPTWKOk&list=PL-osiE80TeTt66h8cVpmbayBKlMTuS55y&index=7
-'''
-
+# Get the FLAG from the environment variable, or use a default value
 FLAG = os.getenv("FLAG", "encryptCTF{comments_&_indentations_makes_johnny_a_good_programme}")
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/encryptCTF', methods=["GET"])
 def getflag():
-	    return jsonify({
-	        'flag': FLAG
-	    })
-	
+    return jsonify({'flag': FLAG})
+
+# Don't use app.run() in production, Vercel will handle the server
 if __name__ == '__main__':
     app.run(debug=False)
